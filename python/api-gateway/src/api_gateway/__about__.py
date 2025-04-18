e09ec43c05daa60e90e59350d3609abeb20d2566
David Rommedahl <david.rommedahl@ericsson.com>
2025-04-18 15:27:58 +0200

    First commit

---
+++ python/api-gateway/src/api_gateway/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present David Rommedahl <david.rommedahl@ericsson.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ python/api-gateway/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present David Rommedahl <david.rommedahl@ericsson.com>
+#
+# SPDX-License-Identifier: MIT
