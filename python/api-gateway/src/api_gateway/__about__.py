326beedb1c708c8b3f186603b52b6ac958ede690
David Rommedahl <david.rommedahl@ericsson.com>
2026-02-23 16:46:08 +0100

    refactor: removed unused folder

--- python/api-gateway/src/api_gateway/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present David Rommedahl <david.rommedahl@ericsson.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- python/api-gateway/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present David Rommedahl <david.rommedahl@ericsson.com>
-#
-# SPDX-License-Identifier: MIT
